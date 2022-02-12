c741910c0f535299263256c6e7cd5f0ffc30330e
dewe <ava6969@rit.edu>
2022-02-12 17:01:47 -0500

    added dep libs

---
+++ a2c_ppo_acktr/a2c_ppo_acktr/envs.py
@@ -0,0 +1,178 @@
+import os
+from collections import OrderedDict
+
+import gym
+import numpy as np
+import torch
+from gym.spaces.box import Box
+from gym.wrappers.clip_action import ClipAction
+from stable_baselines3.common.atari_wrappers import (ClipRewardEnv,
+                                                     EpisodicLifeEnv,
+                                                     FireResetEnv,
+                                                     MaxAndSkipEnv,
+                                                     NoopResetEnv, WarpFrame)
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import (DummyVecEnv, SubprocVecEnv,
+                                              VecEnvWrapper)
+from stable_baselines3.common.vec_env.vec_normalize import \
+    VecNormalize as VecNormalize_
+
+try:
+    import dmc2gym
+except ImportError:
+    pass
+
+try:
+    import roboschool
+except ImportError:
+    pass
+
+try:
+    import pybullet_envs
+except ImportError:
+    pass
+
+
+def make_env(env_id, seed, rank, log_dir, allow_early_resets):
+    def _thunk():
+        env = gym.make(env_id)
+        env.seed(seed + rank)
+
+        if str(env.__class__.__name__).find('TimeLimit') >= 0:
+            env = TimeLimitMask(env)
+
+        if log_dir is not None:
+            env = Monitor(env,
+                          os.path.join(log_dir, str(rank)),
+                          allow_early_resets=allow_early_resets)
+        return env
+
+    return _thunk
+
+
+def make_vec_envs(env_name,
+                  seed,
+                  num_processes,
+                  gamma,
+                  log_dir,
+                  device,
+                  allow_early_resets,
+                  num_frame_stack=None):
+
+    envs = [
+        make_env(env_name, seed, i, log_dir, allow_early_resets)
+        for i in range(num_processes)
+    ]
+
+    if len(envs) > 1:
+        envs = SubprocVecEnv(envs)
+    else:
+        envs = DummyVecEnv(envs)
+
+    envs = VecNormalize(envs, gamma=gamma)
+    envs = VecPyTorch(envs, device)
+
+    return envs
+
+
+# Checks whether done was caused my timit limits or not
+class TimeLimitMask(gym.Wrapper):
+    def step(self, action):
+        obs, rew, done, info = self.env.step(action)
+        if done and self.env._max_episode_steps == self.env._elapsed_steps:
+            info['bad_transition'] = True
+
+        return obs, rew, done, info
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+
+# Can be used to test recurrent policies for Reacher-v2
+class MaskGoal(gym.ObservationWrapper):
+    def observation(self, observation):
+        if self.env._elapsed_steps > 0:
+            observation[-2:] = 0
+        return observation
+
+
+class TransposeObs(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        """
+        Transpose observation space (base class)
+        """
+        super(TransposeObs, self).__init__(env)
+
+
+class TransposeImage(TransposeObs):
+    def __init__(self, env=None, op=[2, 0, 1]):
+        """
+        Transpose observation space for images
+        """
+        super(TransposeImage, self).__init__(env)
+        assert len(op) == 3, "Error: Operation, " + str(op) + ", must be dim3"
+        self.op = op
+        obs_shape = self.observation_space.shape
+        self.observation_space = Box(
+            self.observation_space.low[0, 0, 0],
+            self.observation_space.high[0, 0, 0], [
+                obs_shape[self.op[0]], obs_shape[self.op[1]],
+                obs_shape[self.op[2]]
+            ],
+            dtype=self.observation_space.dtype)
+
+    def observation(self, ob):
+        return ob.transpose(self.op[0], self.op[1], self.op[2])
+
+
+class VecPyTorch(VecEnvWrapper):
+    def __init__(self, venv, device):
+        """Return only every `skip`-th frame"""
+        super(VecPyTorch, self).__init__(venv)
+        self.device = device
+        # TODO: Fix data types
+
+    def observation(self, obs):
+        if isinstance(obs, OrderedDict):
+            return {k: torch.from_numpy(v).float().to(self.device) for k, v in obs.items()}
+        else:
+            return torch.from_numpy(obs).float().to(self.device)
+
+    def reset(self):
+        return self.observation(self.venv.reset())
+
+    def step_async(self, actions):
+        if isinstance(actions, torch.LongTensor):
+            # Squeeze the dimension for discrete actions
+            actions = actions.squeeze(1)
+        actions = actions.cpu().numpy()
+        self.venv.step_async(actions)
+
+    def step_wait(self):
+        obs, reward, done, info = self.venv.step_wait()
+        obs = self.observation(obs)
+        reward = torch.from_numpy(reward).unsqueeze(dim=1).float()
+        return obs, reward, done, info
+
+
+class VecNormalize(VecNormalize_):
+    def __init__(self, *args, **kwargs):
+        super(VecNormalize, self).__init__(*args, **kwargs)
+        self.training = True
+
+    def _obfilt(self, obs, update=True):
+        if self.obs_rms:
+            if self.training and update:
+                self.obs_rms.update(obs)
+            obs = np.clip((obs - self.obs_rms.mean) /
+                          np.sqrt(self.obs_rms.var + self.epsilon),
+                          -self.clip_obs, self.clip_obs)
+            return obs
+        else:
+            return obs
+
+    def train(self):
+        self.training = True
+
+    def eval(self):
+        self.training = False
